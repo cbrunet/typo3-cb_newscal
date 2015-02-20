17cb66d92a5876e2d13b3e96740e740c1a4c114c
Charles Brunet <charles@cbrunet.net>
2015-02-19 22:06:37 -0500

    Tested TYPO3 7.0 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.99.99',
 			'news' => '3.0.0-3.1.99',
 		),
 		'conflicts' => array(
