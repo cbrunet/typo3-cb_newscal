5a00fbda655fafd63de26b6886ef36e6cb186f15
Charles Brunet <charles@cbrunet.net>
2015-02-24 11:41:17 -0500

    Fixed incompatibility with TYPO3 7.1

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,7 @@ $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['switchableControllerActions']['newIt
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['News']['plugins']['Pi1']['controllers']['Newscal']['actions']['0'] = 'calendar';
 
 // Display custom information for Preview in Page module
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['news_pi1']['newscal'] = '\Cbrunet\CbNewscal\Hooks\CmsLayout->getExtensionSummary';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['news_pi1']['newscal'] = 'Cbrunet\CbNewscal\Hooks\CmsLayout->getExtensionSummary';
 
 // Update fields in the flexform
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['Hooks/T3libBefunc.php']['updateFlexforms'][] = '\Cbrunet\CbNewscal\Hooks\T3libBefunc->updateFlexforms';
+$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['Hooks/T3libBefunc.php']['updateFlexforms'][] = 'Cbrunet\CbNewscal\Hooks\T3libBefunc->updateFlexforms';
